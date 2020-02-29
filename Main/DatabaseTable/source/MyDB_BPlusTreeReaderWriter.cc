3c9e0275eba597edf9d8af7d81cde3f5be11b724
Shiyu Wang <wsy_samuel@outlook.com>
2020-02-28 21:36:47 -0600

    update split method

--- Main/DatabaseTable/source/MyDB_BPlusTreeReaderWriter.cc
+++ Main/DatabaseTable/source/MyDB_BPlusTreeReaderWriter.cc
@@ -107,6 +107,7 @@ bool MyDB_BPlusTreeReaderWriter :: discoverPages (int curNode, vector <MyDB_Page
 }
 
 void MyDB_BPlusTreeReaderWriter :: append (MyDB_RecordPtr appendMe) {
+<<<<<<< Updated upstream
     if(rootLocation==-1){//empty table
         //root page
         MyDB_PageReaderWriter mPageRW = (*this)[0]; //page zero already exist. Just use it.
@@ -183,6 +184,96 @@ MyDB_RecordPtr MyDB_BPlusTreeReaderWriter :: append (int whichPage, MyDB_RecordP
             return split(mPageRW, mRec);
         }
     }
+=======
+    // call append(root, rec), traverse until find a leaf node using page type
+    //if the helper function indicates that a split has happened 
+    // then this method needs to handle this by creating a new root
+    // that contains pointers to both the old root and the result of the split.
+}
+
+MyDB_RecordPtr MyDB_BPlusTreeReaderWriter :: split (MyDB_PageReaderWriter splitMe, MyDB_RecordPtr andMe) {
+    MyDB_RecordIteratorAltPtr itr = splitMe.getIteratorAlt();
+    MyDB_INRecordPtr middleRec = getINRecord();
+    // create a new page
+    int newPage =  this->getTable()->lastPage() + 1;
+    this->getTable()->setLastPage(newPage);
+
+    if (splitMe.getType() == RegularPage) {
+        // Leaf Page
+        // find number of records in this page
+
+        MyDB_RecordPtr curr = this->getEmptyRecord();
+        // construct comparator
+        function<bool()> comparatorLeaf = buildComparator(andMe, curr);
+        // store every record in the old page
+        vector<MyDB_RecordPtr> oldRecList;
+        int counter = 0, found = 0;
+        while (itr->advance()) {
+            counter++;
+            itr->getCurrent(curr);
+            if (!found) {
+                if (comparatorLeaf()) {
+                    // we find andMe's pos
+                    oldRecList.push_back(andMe);
+                    found = 1;
+                    counter++;
+                }
+            }
+            oldRecList.push_back(curr);
+        }
+        // [ r1, r2 .... andMe .... rn] n+1 records
+
+        splitMe.clear();
+
+        // append first half of records in current page to the new page, second half to the old page
+        int midPos = counter / 2;
+        for (int i = 1; i <= counter; i++) {
+            // now check myPos is in the first half or second half
+            if (i <= midPos) {
+                (*this)[newPage].append(oldRecList[i-1]);
+                if (i == midPos) {
+                    middleRec->setKey(oldRecList[i-1]->getAtt(0));
+                }
+            } else {
+                splitMe.append(oldRecList[i-1]);
+            }
+        }
+        // modify pointers
+        middleRec->setPtr(newPage);
+        // return the IN Record of the new page
+    } else if (splitMe.getType() == DirectoryPage) {
+        // Inner Page
+        MyDB_INRecordPtr curr = getINRecord();
+        // store every record in the old page
+        vector<MyDB_INRecordPtr> innerNodes;
+        int counter = 0;
+        while (itr->advance()) {
+            counter++;
+            itr->getCurrent(curr);
+            innerNodes.push_back(curr);
+        }
+        splitMe.clear();
+        // kick the middle one to the parent inner page
+        int mid = counter / 2;
+        for (int i = 1; i <= counter; i++) {
+            if (i < mid) {
+                (*this)[newPage].append(innerNodes[i-1]);
+            } else if (i == mid) {
+                middleRec.set
+            } else {
+                splitMe.append(innerNodes[i-1]);
+            }
+        }
+    }
+	return middleRec;
+}
+
+MyDB_RecordPtr MyDB_BPlusTreeReaderWriter :: append (int, MyDB_RecordPtr) {
+    // 2 case
+    // 1. size > pagesize/recordsize
+    // 2. size < pagesize/recordsize append rec to last rec, done
+	return nullptr;
+>>>>>>> Stashed changes
 }
 
 MyDB_INRecordPtr MyDB_BPlusTreeReaderWriter :: getINRecord () {
